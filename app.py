c08da241b202ea7ca9650ce1e2309351d9a943a7
Michał Świdziński <michalswidzinski01@gmail.com>
2022-11-03 19:00:32 +0100

    Add menu, functions for going through the database as well as add new donors

--- app.py
+++ app.py
@@ -1,9 +1,75 @@
 import database
 
+MENU_PROMPT = """ --Blood Barnk Database --
+
+Options:
+
+1) Add a new donor 
+2) Search all donors
+3) Search donors by name
+4) Search donors by surname
+5) Search donors by age
+6) Search donors by sex
+7) Search donors by type
+8) Check blood quantity by type
+9) Exit
+
+Select: """
+
 
 def menu():
     connection = database.connect_db()
     database.create_table(connection)
+    
+    while (user_input := input(MENU_PROMPT)) != "9":
+        if user_input == "1":
+            name = input("Enter donor's name: ")
+            surname = input("Enter donor's surname: ")
+            age = int(input("Enter donor's age: "))
+            sex = input("Enter donor's sex: ")
+            bloodtype = input("Enter donor's blood type: ")
+            quantity = int(input("Enter quantity donated: "))
+            database.add_donor(connection, name, surname, age, sex, bloodtype, quantity)
+        elif user_input == "2":
+            list_of_donors = database.get_all_donors(connection)
+            for donor in list_of_donors:
+                print(donor)
+        elif user_input == "3":
+            if user_input == "3":
+                name = input("Enter donor's name: ")
+                get_donors_by_name = database.get_donors_by_name(connection, name)
+                for name in get_donors_by_name:
+                    print(name)
+        elif user_input == "4":
+            if user_input == "4":
+                surname = input("Enter donor's surname: ")
+                get_donors_by_surname = database.get_donors_by_surname(connection, surname)
+                for surname in get_donors_by_surname:
+                    print(surname)
+        elif user_input == "5":
+            if user_input == "5":
+                age = int(input("Enter donor's age: "))
+                get_donors_by_age = database.get_donors_by_age(connection, age)
+                for age in get_donors_by_age:
+                    print(age)
+        elif user_input == "6":
+            if user_input == "6":
+                sex = input("Enter donor's sex: ")
+                get_donors_by_sex = database.get_donors_by_sex(connection, sex)
+                for sex in get_donors_by_sex:
+                    print(sex)
+        elif user_input == "7":
+            if user_input == "7":
+                bloodtype = input("Enter donor's blood type: ")
+                get_donors_by_type = database.get_donors_by_type(connection, bloodtype)
+                for bloodtype in get_donors_by_type:
+                    print(bloodtype)
+        elif user_input == "8":
+            blood_quantity = database.get_type_quantity(connection)
+            for bloodtype in blood_quantity:
+                print(bloodtype)
+        else:
+            print("Invalid input")
 
-
-
+        
+menu()
--- database.py
+++ database.py
@@ -1,17 +1,22 @@
 import sqlite3
 
 
-CREATE_BLOODBANK_TABLE = "CREATE TABLE donors (id INTEGER PRIMERY KEY, name TEXT, surname TEXT, age INTEGER, sex TEXT, type TEXT);"
+CREATE_BLOODBANK_TABLE = "CREATE TABLE IF NOT EXISTS donors (id INTEGER PRIMERY KEY, name TEXT, surname TEXT, age INTEGER, sex TEXT, bloodtype TEXT, quantity INTEGER);"
 
-INSERT_DONOR = "INSERT INTO donors (name, surname, age, sex, type) VALUES(?, ?, ?, ?, ?);"
+INSERT_DONOR = "INSERT INTO donors (name, surname, age, sex, bloodtype, quantity) VALUES(?, ?, ?, ?, ?, ?);"
 
 GET_ALL_DONORS = "SELECT * FROM donors;"
 GET_DONORS_BY_NAME = "SELECT * FROM donors WHERE name = ?;"
 GET_DONORS_BY_SURNAME = "SELECT * FROM donors WHERE surname = ?;"
 GET_DONORS_BY_AGE = "SELECT * FROM donors WHERE age = ?;"
 GET_DONORS_BY_SEX = "SELECT * FROM donors WHERE sex = ?;"
-GET_DONORS_BY_TYPE = "SELECT * FROM donors WHERE type = ?;"
-
+GET_DONORS_BY_TYPE = "SELECT * FROM donors WHERE bloodtype = ?;"
+GET_TYPE_QUANTITY = """
+                    SELECT bloodtype,
+                    SUM (quantity)
+                    FROM donors
+                    GROUP BY bloodtype;
+                    """
 
 def connect_db():
     return sqlite3.connect("blood-bank-sql/bloodbank.db")
@@ -20,31 +25,36 @@ def create_table(connection):
     with connection:
         connection.execute(CREATE_BLOODBANK_TABLE)
 
-def add_donor(connection, name, surname, age, sex, type):
+def add_donor(connection, name, surname, age, sex, bloodtype, quantity):
     with connection:
-        connection.execute(INSERT_DONOR, (name, surname, age, sex, type))
+        connection.execute(INSERT_DONOR, (name, surname, age, sex, bloodtype, quantity,))
 
 def get_all_donors(connection):
     with connection:
         return connection.execute(GET_ALL_DONORS).fetchall()
 
+def get_type_quantity(connection):
+    with connection:
+        return connection.execute(GET_TYPE_QUANTITY).fetchall()
+
 def get_donors_by_name(connection, name):
     with connection:
-        return connection.execute(GET_DONORS_BY_NAME, (name)).fetchall()
+        return connection.execute(GET_DONORS_BY_NAME, (name,)).fetchall()
 
 def get_donors_by_surname(connection, surname):
     with connection:
-        return connection.execute(GET_DONORS_BY_SURNAME, (surname)).fetchall()
+        return connection.execute(GET_DONORS_BY_SURNAME, (surname,)).fetchall()
 
 def get_donors_by_age(connection, age):
     with connection:
-        return connection.execute(GET_DONORS_BY_AGE, (age)).fetchall()
+        return connection.execute(GET_DONORS_BY_AGE, (age,)).fetchall()
 
 def get_donors_by_sex(connection, sex):
     with connection:
-        return connection.execute(GET_DONORS_BY_SEX, (sex)).fetchall()
+        return connection.execute(GET_DONORS_BY_SEX, (sex,)).fetchall()
 
-def get_donors_by_type(connection, type):
+def get_donors_by_type(connection, bloodtype):
     with connection:
-        return connection.execute(GET_DONORS_BY_TYPE, (type)).fetchall()
+        return connection.execute(GET_DONORS_BY_TYPE, (bloodtype,)).fetchall()
+
 
