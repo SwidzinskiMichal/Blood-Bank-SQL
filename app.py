8381a853ea54c76bc3ae739911ca98e3991bd2e2
Michał Świdziński <michalswidzinski01@gmail.com>
2022-11-09 17:23:21 +0100

    Rework the search functions and combined it into one, add tests for new search function

--- app.py
+++ app.py
@@ -5,16 +5,11 @@ MENU_PROMPT = """ --Blood Barnk Database --
 Options:
 
 1) Add a new donor 
-2) Search all donors
-3) Search donors by id
-4) Search donors by name
-5) Search donors by surname
-6) Search donors by age
-7) Search donors by sex
-8) Search donors by type
-9) Check blood quantity by type
-10) Update donor's blood quantity
-11) Delete donor from database
+2) Get all donors
+3) Search donors
+4) Check blood quantity by type
+5) Update donor's blood quantity
+6) Delete donor from database
 0) Exit
 
 Select: """
@@ -30,22 +25,12 @@ def menu():
         elif user_input == "2":
             list_donors(connection)
         elif user_input == "3":
-            search_donors_by_id(connection)
+            search_donors(connection)
         elif user_input == "4":
-            search_donors_by_name(connection)
-        elif user_input == "5":
-            search_donors_by_surname(connection)
-        elif user_input == "6":
-            search_donors_by_age(connection)
-        elif user_input == "7":
-            search_donors_by_sex(connection)
-        elif user_input == "8":
-            search_donors_by_bloodtype(connection)
-        elif user_input == "9":
             check_blood_quanity(connection)
-        elif user_input == "10":
+        elif user_input == "5":
             update_bloods_quantity(connection)
-        elif user_input == "11":
+        elif user_input == "6":
             remove_donor_from_db(connection)
         else:
             print("Invalid input")
@@ -65,41 +50,14 @@ def list_donors(connection):
     for donor in list_donors:
         print(donor)   
 
-def search_donors_by_id(connection):
-    id = input("Enter donor's id: ")
-    get_donors_by_id = database.fetch_donors_by_id(connection, id)
-    for id in get_donors_by_id:
-        print(id)
-
-def search_donors_by_name(connection):
-    name = input("Enter donor's name: ")
-    get_donors_by_name = database.fetch_donors_by_name(connection, name)
-    for name in get_donors_by_name:
-        print(name)
-
-def search_donors_by_surname(connection):
-    surname = input("Enter donor's surname: ")
-    get_donors_by_surname = database.fetch_donors_by_surname(connection, surname)
-    for surname in get_donors_by_surname:
-        print(surname)
-
-def search_donors_by_age(connection):
-    age = int(input("Enter donor's age: "))
-    get_donors_by_age = database.fetch_donors_by_age(connection, age)
-    for age in get_donors_by_age:
-        print(age)
-
-def search_donors_by_sex(connection):
-    sex = input("Enter donor's sex: ")
-    get_donors_by_sex = database.fetch_donors_by_sex(connection, sex)
-    for sex in get_donors_by_sex:
-        print(sex)
-
-def search_donors_by_bloodtype(connection):
-    bloodtype = input("Enter donor's blood type: ")
-    get_donors_by_type = database.fetch_donors_by_type(connection, bloodtype)
-    for bloodtype in get_donors_by_type:
-        print(bloodtype)
+def search_donors(connection):
+    print("Please enter search paramiters in lower case and with inverted commas (ex. name = 'John').")
+    print("Multiple paramiters searched together need to be separated with AND (ex. name = 'John' AND surname = 'Adams')")
+    print("Allowed search paramiters are name, surname, age, sex and bloodtype")
+    search_paramiters = input("Enter paramiters: ")
+    get_donors = database.custom_search(connection, search_paramiters)
+    for donor in get_donors:
+        print(donor)
 
 def check_blood_quanity(connection):
     blood_quantity = database.fetch_type_quantity(connection)
--- database.py
+++ database.py
@@ -1,17 +1,12 @@
 import sqlite3
 
 
-CREATE_BLOODBANK_TABLE = "CREATE TABLE IF NOT EXISTS donors (id INTEGER PRIMARY KEY, name TEXT, surname TEXT, age INTEGER, sex TEXT, bloodtype TEXT, quantity INTEGER);"
+CREATE_BLOODBANK_TABLE = "CREATE TABLE IF NOT EXISTS donors (id INTEGER PRIMARY KEY, name TEXT, surname TEXT, age TEXT, sex TEXT, bloodtype TEXT, quantity INTEGER);"
 
 INSERT_DONOR = "INSERT INTO donors (name, surname, age, sex, bloodtype, quantity) VALUES (?, ?, ?, ?, ?, ?);"
 
 GET_ALL_DONORS = "SELECT * FROM donors;"
-GET_DONOR_BY_ID = "SELECT * FROM donors WHERE id = ?;"
-GET_DONORS_BY_NAME = "SELECT * FROM donors WHERE name = ?;"
-GET_DONORS_BY_SURNAME = "SELECT * FROM donors WHERE surname = ?;"
-GET_DONORS_BY_AGE = "SELECT * FROM donors WHERE age = ?;"
-GET_DONORS_BY_SEX = "SELECT * FROM donors WHERE sex = ?;"
-GET_DONORS_BY_TYPE = "SELECT * FROM donors WHERE bloodtype = ?;"
+
 GET_TYPE_QUANTITY = """
                     SELECT bloodtype,
                     SUM (quantity)
@@ -40,26 +35,8 @@ def add_donor(cursor, name, surname, age, sex, bloodtype, quantity):
 def fetch_all_donors(cursor):
     return cursor.execute(GET_ALL_DONORS).fetchall()
 
-def fetch_donors_by_id(cursor, id):
-    return cursor.execute(GET_DONOR_BY_ID, (id,)).fetchone()
-
-def fetch_type_quantity(cursor):
-    return cursor.execute(GET_TYPE_QUANTITY).fetchall()
-
-def fetch_donors_by_name(cursor, name):
-    return cursor.execute(GET_DONORS_BY_NAME, (name,)).fetchall()
-
-def fetch_donors_by_surname(cursor, surname):
-    return cursor.execute(GET_DONORS_BY_SURNAME, (surname,)).fetchall()
-
-def fetch_donors_by_age(cursor, age):
-    return cursor.execute(GET_DONORS_BY_AGE, (age,)).fetchall()
-
-def fetch_donors_by_sex(cursor, sex):
-    return cursor.execute(GET_DONORS_BY_SEX, (sex,)).fetchall()
-
-def fetch_donors_by_type(cursor, bloodtype):
-    return cursor.execute(GET_DONORS_BY_TYPE, (bloodtype,)).fetchall()
+def custom_search(cursor, search_paramiters):
+    return cursor.execute(f"SELECT * FROM donors WHERE {search_paramiters};").fetchall()
 
 def update_blood_quantity_in_db(cursor, quantity, id):
     cursor.execute(UPDATE_BLOOD_QUANTITY,(quantity, id,))
